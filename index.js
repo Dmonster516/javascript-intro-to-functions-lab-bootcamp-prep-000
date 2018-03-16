e566d88d6dbd274d4562f0924ced04a8662f74e4
kim tran <icycleitaly@gmail.com>
2018-03-16 19:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 function shout(string) {
-  return string.toUpperCase;
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowercase;
+  return string.toLowerCase();
 }
 
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+funnction sayHiToGrandma(string){
+  if string === string.toLowerCase(){
+    console.log("I can't hear you!")
+  }
+  else {
+    coso
+  }
+}
