45bc48be226484e3d610248b6162110113eb91de
kim tran <icycleitaly@gmail.com>
2018-03-16 19:58:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
     console.log("I can't hear you!")
   }
+  else if(string==="I love you, Grandma"){
+    console.log("I love you too.")
+  }
   else {
     console.log("YES INDEED")
   }
